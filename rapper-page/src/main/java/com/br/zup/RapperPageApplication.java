7d3a80eea15f9ec075d6141ce5d387ae40e5df00
Tiago Souza <tiago.souza@zup.com.br>
2019-09-24 15:24:13 -0300

    Criar projeto de uma página com infos de rappers

---
+++ rapper-page/src/main/java/com/br/zup/RapperPageApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapperPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapperPageApplication.class, args);
+	}
+
+}
